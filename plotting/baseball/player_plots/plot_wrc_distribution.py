2970f869357e67455bb664f9929b3d87508385f5
Sohraub Pazuki <sohraub.pazuki@arctiq.ca>
2025-04-02 17:43:34 -0400

    begun work on some baseball stuff, specifically wrc+ distribution plot

---
+++ plotting/baseball/player_plots/plot_wrc_distribution.py
@@ -0,0 +1,37 @@
+import argparse
+from datetime import datetime
+import pybaseball
+
+from plotting.base_plots.violin import ViolinPlot
+
+
+
+def main(year, qual, team):
+    data = pybaseball.batting_stats(year, qual=qual)[['Team', 'Name', 'wRC+']]
+    data['team'] = data['Team']
+
+    plot = ViolinPlot(dataframe=data,
+                      filename=f'{team}_wrc.png',
+                      column='wRC+',
+                      team=team,
+                      y_label='wRC+',
+                      title=f'{team} Hitters by wRC+',
+                      subtitle=f'Compared to league distribution, min. {qual} PAs')
+    
+    plot.make_plot()
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-y', '--year', type=int, default=datetime.now().year,
+                        help='Year for which to get data, defaults to current year')
+    parser.add_argument('-q', '--qual', default=15, type=int,
+                        help='Minimum plate appearances to qualify in query, defaults to 15')
+    parser.add_argument('-t', '--team', required=True, type=str,
+                        help='Team for which players should be highlighted.')
+    args = parser.parse_args()
+
+    main(year=args.year, qual=args.qual, team=args.team)
+
