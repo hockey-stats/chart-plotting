0e5862bef18c8dc7ec3a94ec61e3d8dec3dda05e
Sohraub Pazuki <sohraub.pazuki@arctiq.ca>
2025-06-21 19:41:44 -0400

    checkpoint, gotta think on things

--- processing/for_fantasy_baseball/get_free_agent_summary.py
+++ processing/for_fantasy_baseball/get_free_agent_summary.py
@@ -99,7 +99,7 @@ def get_players_from_own_team(position: str, league: yfa.League, session: OAuth2
         if position in player['eligible_positions']:
             players.append(player['player_id'])
 
-    if position in {'1B', '2B', '3B', 'C', 'OF', 'SS'}:
+    if position in {'1B', '2B', '3B', 'C', 'OF', 'SS', 'Util'}:
         df = collect_batter_stats(players, league)
     else:
         df = collect_pitcher_stats(players, league, position)
@@ -140,9 +140,13 @@ def collect_pitcher_stats(player_ids: list, league: yfa.League, position: str) -
     for player_stats, term in zip([player_stats_week, player_stats_month, player_stats_season],
                                   ['week', 'month', 'season']):
         for details, stats in zip(player_details, player_stats):
-            if stats['IP'] == 0.0 or stats['IP'] == '-':
-                # Skip players without any innings pitched
-                continue
+            try:
+                if stats['IP'] == 0.0 or stats['IP'] == '-':
+                    # Skip players without any innings pitched
+                    continue
+            except KeyError as e:
+                print(details, stats)
+                raise e
             p_dict['name'].append(unidecode(details['name']['full']))
             p_dict['team'].append(standardize_team_names(details['editorial_team_abbr']))
             p_dict['positions'].append(','.join([pos['position'] for pos in \
@@ -167,7 +171,7 @@ def collect_pitcher_stats(player_ids: list, league: yfa.League, position: str) -
     if position == 'SP':
         # If looking for starters, remove every pitcher with 0 starts
         p_df = p_df.remove(pl.col("GS") == 0)
-    else:
+    elif position == 'RP':
         # If looking for relievers, remove every pitcher with as many starts as appearances
         p_df = p_df.remove(pl.col("GS") == pl.col("G"))
 
@@ -210,6 +214,7 @@ def collect_batter_stats(player_ids: list, league: yfa.League) -> pl.DataFrame:
         "name": [],
         "team": [],
         "positions": [],
+        "r": [],
         "ab": [],
         "avg": [],
         "hr": [],
@@ -221,16 +226,21 @@ def collect_batter_stats(player_ids: list, league: yfa.League) -> pl.DataFrame:
     for player_stats, term in zip([player_stats_week, player_stats_month, player_stats_season],
                                   ['week', 'month', 'season']):
         for details, stats in zip(player_details, player_stats):
-            if stats['AVG'] == '-':
-                # Skip players without any at-bats.
-                continue
+            try:
+                if stats['AVG'] == '-':
+                    # Skip players without any at-bats.
+                    continue
+            except KeyError as e:
+                print(details, stats)
+                raise e
+
             p_dict['name'].append(unidecode(details['name']['full']))
             p_dict['team'].append(standardize_team_names(details['editorial_team_abbr']))
             p_dict['positions'].append(','.join([pos['position'] \
                                                 for pos in details['eligible_positions'] \
                                                 if pos['position'] != 'Util']))
             p_dict['ab'].append(int(stats['H/AB'].split('/')[-1]))
-
+            p_dict['r'].append(int(stats['R']))
             p_dict['avg'].append(stats['AVG'])
             p_dict['hr'].append(int(stats['HR']))
             p_dict['rbi'].append(int(stats['RBI']))
@@ -398,6 +408,176 @@ def dashboard_main():
         main(position=pos, output_filename=f'fantasy_data_{pos}.csv')
 
 
+def get_z_scores():
+    session = create_session()
+    game = yfa.Game(session, 'mlb')
+    league = yfa.League(session, game.league_ids()[1])
+
+    taken = league.taken_players()
+
+    # Batters
+    batter_ids = [p['player_id'] for p in taken if p['position_type'] == 'B']
+
+    batters = league.free_agents('Util')
+    batter_ids.extend([p['player_id'] for p in batters])
+
+    b_df = collect_batter_stats(batter_ids, league)
+
+    batter_df = compute_z_scores(b_df, player_type='batters')
+
+
+    # Pitchers
+    pitcher_ids = [p['player_id'] for p in taken if p['position_type'] == 'P']
+    pitchers = league.free_agents('P')
+    pitcher_ids.extend([p['player_id'] for p in pitchers])
+
+    p_df = collect_pitcher_stats(pitcher_ids, league, position='P')
+
+    pitcher_df = compute_z_scores(p_df, player_type='pitchers')
+
+
+    free_agent_batters = list(set([p['name'] for p in batters]))
+    free_agent_pitchers = list(set([p['name'] for p in pitchers]))
+
+    my_team = list(get_players_from_own_team(position='Util',
+                                             league=league,
+                                             session=session)['name']) + \
+              list(get_players_from_own_team(position='P',
+                                             league=league,
+                                             session=session)['name'])
+
+    my_team = list(set(my_team))
+
+    # Filter players that are on other teams
+    batter_df = filter_taken(batter_df, free_agent_batters, my_team)
+    pitcher_df = filter_taken(pitcher_df, free_agent_pitchers, my_team)
+
+    # Rename columns to be more presentable
+    batter_df = batter_df.rename({
+        "name": "Name",
+        "team": "Team",
+        "positions": "Position(s)",
+        "avg": "AVG",
+        "ab": "ABs",
+        "hr": "HRs",
+        "rbi": "RBIs",
+        "r": "Runs",
+        "sb": "SBs"
+    })
+    pitcher_df = pitcher_df.rename({
+        "name": "Name",
+        "team": "Team",
+        "positions": "Position(s)",
+        "ip": "IP",
+        "k": "Ks",
+        "w": "Ws",
+        "sv": "SVs",
+        "era": "ERA",
+        "whip": "WHIP",
+    })
+        
+    # Save output
+    pitcher_df.write_csv('pitcher_data.csv')
+    batter_df.write_csv('batter_data.csv')
+
+
+
+def filter_taken(df: pl.DataFrame, free_agents: list[str], my_team: list[str]) -> pl.DataFrame:
+    """
+    Removes from a player DF every player that's on a team which is not my team, and then adds 
+    a column for boolean values corresponding to whether or not a player is on my team.
+
+    :param pl.DataFrame df: DataFrame containing stats for all players
+    :param set[str] free_agents: Set of names of players which are free agents
+    :param set[str] my_team: Set of names of players on my team.
+    :return pl.DataFrame: DataFrame with taken players removed and 'on_team' column added.
+    """
+
+    # Filter out players that are already taken
+    df = df.filter(pl.col('name').is_in(free_agents + my_team))
+
+    # Also filter out any injured players
+    df = df.filter(pl.col('positions').str.contains('IL').not_())
+
+    # Set 'on_team' to True if player is on my team, False otherwise
+    df = df.with_columns(
+        pl.when(pl.col('name').str.contains_any(my_team))
+            .then(pl.lit(True))
+            .otherwise(pl.lit(False))
+            .alias('on_team')
+    )
+
+    return df
+
+
+
+
+
+def compute_z_scores(batter_df: pl.DataFrame, player_type: str) -> pl.DataFrame:
+
+    """
+    Computes z-scores for each column in the DataFrame and returns one with an extra column
+    for the average of these z-scores for each term.
+
+    :param pl.DataFrame batter_df: Raw statistics for each player.
+    :param str player_type: Either 'batters' or 'pitchers'
+    :return pl.DataFrame: Provided DataFrame with z-scores added in.
+    """
+    # Dict to store DataFrames for each term
+    dfs = dict()
+
+    if player_type == 'batters':
+        columns = ['avg', 'r', 'hr', 'rbi', 'sb', 'wRC+', 'xwOBA']
+    else:
+        columns = ['era', 'whip', 'k', 'w', 'sv', 'Stuff+', 'K-BB%']
+
+    min_thresholds = {
+        "batters": {
+            "week": 
+        }
+    }
+
+    for term in ['season', 'month', 'week']:
+        # Compute z-scores seperately for each term
+        x = batter_df.filter(pl.col('term') == term)
+
+        for col in columns:
+            mult = -1 if col in {'era', 'whip'} else 1
+            x = x.with_columns(
+                (((pl.col(col) - pl.mean(col)) / pl.std(col)) * mult).alias(f"z_{col}")
+            )
+
+        if player_type == 'batters':
+            x = x.with_columns(
+                ((pl.col('z_avg') +\
+                  pl.col('z_hr') +\
+                  pl.col('z_rbi') +\
+                  pl.col('z_sb') +\
+                  pl.col('z_r') +\
+                  pl.col('z_wRC+') +\
+                  pl.col('z_xwOBA')) / 7).alias('z_total')
+            )
+        else:
+            x = x.with_columns(
+                ((pl.col('z_era') +\
+                  pl.col('z_whip') +\
+                  pl.col('z_k') +\
+                  pl.col('z_w') +\
+                  pl.col('z_sv') +\
+                  pl.col('z_Stuff+') +\
+                  pl.col('z_K-BB%')) / 7).alias('z_total')
+            )
+
+        drop_columns = [f'z_{col}' for col in columns]
+        x = x.drop(drop_columns)
+        dfs[term] = x.clone()
+
+    final = pl.concat(list(dfs.values()))
+
+    return final
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--position', type=str, default="",
@@ -409,7 +589,9 @@ if __name__ == '__main__':
                              "gathers data for all positions and multiple terms.")
     args = parser.parse_args()
 
-    if args.for_dashboard:
-        dashboard_main()
-    else:
-        main(position=args.position, output_filename=args.output_filename)
+    get_z_scores()
+
+   # if args.for_dashboard:
+   #     dashboard_main()
+   # else:
+   #     main(position=args.position, output_filename=args.output_filename)
